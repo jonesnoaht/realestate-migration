8493b6416f4d48e85fdcb5cc1a476606650061ff
Noah Thomas Jones <noahtjones@gmail.com>
2023-12-26 18:24:19 -0500

    switching map data packages to urbnmapr

--- main.R
+++ main.R
@@ -3,6 +3,13 @@ libs <- list("dplyr", "googlePolylines", "googleway", "ggspatial", "ggrepel",
              "ggplot2", "rjson", "rlang", "stringr", "tibble", "tidyr", "spatial",
              "sp", "sf", "scales", "prettymapr", "purrr", "stats", "graphics",
              "grDevices", "utils", "datasets", "methods", "base", "rnaturalearth",
-             "rnaturalearthdata")
+             "rnaturalearthdata", "ggmap")
 all(lapply(libs, library, character.only = T, logical = T))
-sp::plot(ne_states(country = "united states of america", state = "florida"))
+register_google(key = "AIzaSyDBIxi0iaVEmzKVpGO9CrNTZ9kVxX3_dN8")
+sp::plot(ne_states(country = "united states of america"))
+(rnaturalearthhires::states10 |> filter(admin == "United States of America", 
+                                        type == "State", 
+                                        gn_name == "Florida"))$geometry |> 
+  plot()
+# get_map("florida", zoom = 6, maptype = "terrain-background") |> 
+#   ggmap()
